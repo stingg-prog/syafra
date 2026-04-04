47eab79977a286fc475db78e1f8d033ec36aafa0
Syafra Developer <developer@syafra.com>
2026-04-04 09:44:15 +0530

    Fix Procfile port binding and Celery import for Render

--- syafra/syafra/__init__.py
+++ syafra/syafra/__init__.py
@@ -2,9 +2,11 @@
 SYAFRA Django project package.
 """
 
+# Celery is optional - only import if celery is installed
 try:
+    from celery import Celery
     from .celery import app as celery_app
-except ImportError:  # pragma: no cover - Celery remains optional in local/test envs.
+except ImportError:
     celery_app = None
 
 __all__ = ('celery_app',)
