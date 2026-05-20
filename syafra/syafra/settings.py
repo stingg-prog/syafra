a410b3510769d02dd7cbd377c0da61eca095a43b
Syafra Developer <developer@syafra.com>
2026-05-20 20:04:15 +0530

    Use sqlite fallback for local development

--- syafra/syafra/settings.py
+++ syafra/syafra/settings.py
@@ -188,7 +188,10 @@ TEMPLATES = [
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        default=os.environ.get(
+            "DATABASE_URL",
+            f"sqlite:///{BASE_DIR / 'db.sqlite3'}" if DEBUG else None,
+        )
     )
 }
 
